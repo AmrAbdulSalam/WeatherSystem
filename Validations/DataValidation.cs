7e454d0dd036848de49c781bdca83b9de6809adb
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-20 17:17:05 +0300

    Validtion data type

---
+++ Validations/DataValidation.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WeatherSystem.Validations
+{
+    public static class DataValidation
+    {
+        public static bool IsJson(string input)
+        {
+            return (input.StartsWith("{") && input.EndsWith("}"));
+        }
+    }
+}

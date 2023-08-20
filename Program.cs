1a71b9865e98e8779139c352a6b38288b046d026
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-20 18:13:23 +0300

    Main program to read user json/xml data

--- Program.cs
+++ Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WeatherSystem;
+using WeatherSystem.Factory;
+using WeatherSystem.Models;
+using WeatherSystem.Strategies;
+using WeatherSystem.Validations;
 
+namespace Weather
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            string configurationFile = @"/Users/amr/Desktop/Foothill-Internship/CleanCode/WeatherSystem/configuration.json";
+
+            while (true)
+            {
+                await ReadConfiguration.ReadConfigurationFileAsync(configurationFile);
+                Console.WriteLine("Enter weather data:");
+                var weatherData = Console.ReadLine();
+                WeatherDTO data;
+
+                if (DataValidation.IsJson(weatherData))
+                {
+                    var context = new Context(new JsonStrategy());
+                    data = context.ExecuteStrtegy(weatherData);
+                }
+                else if (DataValidation.IsXml(weatherData))
+                {
+                    var context = new Context(new XmlStrategy());
+                    data = context.ExecuteStrtegy(weatherData);
+                }
+                else
+                {
+                    break;
+                }
+
+                var factory = new ConcreteBotFactory();
+
+
+                var rainBot = factory.GetBot("rain");
+                rainBot.DisplayMessage(data);
+
+                var snowBot = factory.GetBot("snow");
+                snowBot.DisplayMessage(data);
+
+
+                var sunBot = factory.GetBot("sun");
+                sunBot.DisplayMessage(data);
+
+            }
+        }
+    }
+}

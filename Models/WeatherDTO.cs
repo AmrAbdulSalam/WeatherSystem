f39490b8a905da8a364025fb525ff26dd350445d
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-20 17:15:29 +0300

    Adding WeatherDTO to hold user input

---
+++ Models/WeatherDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WeatherSystem.Models
+{
+    public class WeatherDTO
+    {
+        public string Location { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+    }
+}

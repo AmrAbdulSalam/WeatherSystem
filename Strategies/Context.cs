087e151c6f835e494c9939a088ec5791392f6994
AmrAbdulSalam <amrsalam@hotmail.com>
2023-08-20 17:19:45 +0300

    Using strategy design pattern to determine which data type

---
+++ Strategies/Context.cs
@@ -0,0 +1,19 @@
+﻿using WeatherSystem.Models;
+
+namespace WeatherSystem.Strategies
+{
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public Context(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public WeatherDTO ExecuteStrtegy(string input)
+        {
+            return _strategy.ReadData(input);
+        }
+    }
+}
---
+++ Strategies/JsonStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using WeatherSystem.Models;
+
+namespace WeatherSystem.Strategies
+{
+    public class JsonStrategy : IStrategy
+    {
+        public WeatherDTO ReadData(string input)
+        {
+            return JsonSerializer.Deserialize<WeatherDTO>(input);
+        }
+    }
+}
